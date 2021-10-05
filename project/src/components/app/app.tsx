d913fe0a52a7042feb2546ca8525e13c1f923e4b
AleksRomanov <kosmonavt-vavilon@mail.ru>
2021-10-06 02:06:39 +0700

    3) Маршрут по тайным тропам Описание маршрута для компонента страницы 404

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,8 +1,9 @@
-import Main from '../main/main';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Main from '../main/main';
 import Login from '../login/login';
 import Favorites from '../favorites/favorites';
 import CardPropertyNotLogged from '../card-property-not-logged/card-property-not-logged';
+import MainPage404 from '../main-page-404/main-page-404';
 
 type AppProps = {
   placeCardCount: number;
@@ -15,9 +16,20 @@ function App({placeCardCount}: AppProps): JSX.Element {
         <Route path="/" exact>
           <Main placeCardCount={placeCardCount}/>
         </Route>
-        <Route path="/login" exact component={Login}/>
-        <Route path="/favorites" exact component={Favorites}/>
-        <Route path="/offer/:id" exact component={CardPropertyNotLogged}/>
+        <Route path="/login" exact>
+          <Login/>
+        </Route>
+        <Route path="/favorites" exact>
+          <Favorites/>
+        </Route>
+        <Route path="/offer/:id" exact>
+          <CardPropertyNotLogged/>
+        </Route>
+        <Route
+          render={(props) => (
+            <MainPage404/>
+          )}
+        />
       </Switch>
     </BrowserRouter>
   );
