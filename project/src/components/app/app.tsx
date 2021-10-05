5f9b3b7da18cac8ae8d0b548387af4c5000ff94f
AleksRomanov <kosmonavt-vavilon@mail.ru>
2021-10-06 02:01:34 +0700

    3) Маршрут по тайным тропам Описание маршрутов в App

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,12 +1,26 @@
 import Main from '../main/main';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Login from '../login/login';
+import Favorites from '../favorites/favorites';
+import CardPropertyNotLogged from '../card-property-not-logged/card-property-not-logged';
 
 type AppProps = {
   placeCardCount: number;
 }
 
 function App({placeCardCount}: AppProps): JSX.Element {
-  return <Main placeCardCount={placeCardCount} />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <Main placeCardCount={placeCardCount}/>
+        </Route>
+        <Route path="/login" exact component={Login}/>
+        <Route path="/favorites" exact component={Favorites}/>
+        <Route path="/offer/:id" exact component={CardPropertyNotLogged}/>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
