06c98b677de46d4a8f873eb4f198e23ea04b9746
aleksromanov <kosmonavt-vavilon@mail.ru>
2021-11-10 17:37:22 +0700

    6.2 Redux Устранение последних замечаний Done

--- project/src/components/sorting-list/sorting-list.tsx
+++ project/src/components/sorting-list/sorting-list.tsx
@@ -1,14 +1,13 @@
 import 'leaflet/dist/leaflet.css';
 import {SortType} from '../../constants';
-import SortingListItem from '../sorting-list-item/sorting-list-item';
 import {State} from '../../types/state';
 import {connect, ConnectedProps} from 'react-redux';
-import {nanoid} from 'nanoid';
 import {ReactComponent as IconArrowSelect} from '../../static/icon-arrow-select.svg';
 import React from 'react';
 import {Dispatch} from 'redux';
-import {changeSortPanelOpenStatus} from '../../store/action';
 import {ActionsType} from '../../types/action';
+import {changeSortPanelOpenStatus, changeSortType, sortCurrentOffers} from '../../store/action';
+import {nanoid} from 'nanoid';
 
 function mapStateToProps({isSortingListOpen, currentSortType}: State) {
   return ({
@@ -22,27 +21,32 @@ function mapDispatchToProps(dispatch: Dispatch<ActionsType>) {
     onSortPanelClick() {
       dispatch(changeSortPanelOpenStatus());
     },
+    onChangeSortType(type: string) {
+      dispatch(changeSortType(type));
+      dispatch(sortCurrentOffers(type));
+    },
   };
 }
 
 const connector = connect(mapStateToProps, mapDispatchToProps);
 type PropsFromRedux = ConnectedProps<typeof connector>;
 
-function SortingList({isSortingListOpen, onSortPanelClick, currentSortType}: PropsFromRedux): JSX.Element {
+function SortingList({isSortingListOpen, onSortPanelClick, currentSortType, onChangeSortType}: PropsFromRedux): JSX.Element {
+  const handleOptionClick = (sortType: string): void => {
+    onChangeSortType(sortType);
+  };
 
-  function SortingListWindow() {
+  function SortingItems() {
     return (
       <>
-        <ul className={isSortingListOpen ?
-          'places__options places__options--custom places__options--opened' :
-          'places__options places__options--custom'}
-        >
-          {Object.values(SortType).map((sortType) => (
-            <SortingListItem
-              sortType={sortType}
-              key={nanoid()}
-            />))}
-        </ul>
+        {Object.values(SortType).map((sortType) => (
+          <li key={nanoid()} onClick={() => handleOptionClick(sortType)} className={currentSortType === sortType ?
+            'places__option places__option--active' :
+            'places__option'}
+          >
+            {sortType}
+          </li>
+        ))}
       </>
     );
   }
@@ -54,7 +58,12 @@ function SortingList({isSortingListOpen, onSortPanelClick, currentSortType}: Pro
                   {currentSortType}
         <IconArrowSelect/>
               </span>
-      <SortingListWindow/>
+      <ul className={isSortingListOpen ?
+        'places__options places__options--custom places__options--opened' :
+        'places__options places__options--custom'}
+      >
+        <SortingItems/>
+      </ul>
     </>
   );
 }
