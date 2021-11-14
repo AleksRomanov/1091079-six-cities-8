b849d9c8968142405df1eecaa86d0464ef1a61be
aleksromanov <kosmonavt-vavilon@mail.ru>
2021-11-14 19:34:13 +0700

    7.13. Истина где-то на сервере Исправление замечаний наставника. Чистка комментариев Вынос адаптера в отдельную функцию

--- project/src/store/api-actions.ts
+++ project/src/store/api-actions.ts
@@ -3,7 +3,7 @@ import {APIRoute, AuthorizationStatus} from '../constants';
 import {loadOffers, requireAuthorization} from './action';
 import {toast} from 'react-toastify';
 import {OfferType} from '../types/offerType';
-const AUTH_FAIL_MESSAGE = 'Не забудьте авторизоваться';
+const AUTH_FAIL_MESSAGE = 'Пожалуйста авторизуйтесь!';
 
 export const checkAuthAction = (): ThunkActionResult =>
   async (dispatch, _getState, api) => {
@@ -15,30 +15,28 @@ export const checkAuthAction = (): ThunkActionResult =>
     }
   };
 
+const adaptFromServer = (offer: any) => {
+  const adaptOfferFeatures = (offer: any) => {
+    for (let feature in offer) {
+      if (typeof (offer[feature]) === 'object') {
+        adaptOfferFeatures(offer[feature]);
+      } else {
+        let snakeSymbolIndex = feature.indexOf('_');
+        if (snakeSymbolIndex >= 0) {
+          offer[feature.slice(0, snakeSymbolIndex) + feature.slice(++snakeSymbolIndex)[0].toUpperCase() + feature.slice(++snakeSymbolIndex)] = offer[feature];
+          delete offer[feature];
+        }
+      }
+    }
+  }
+  adaptOfferFeatures(offer);
+  return offer;
+}
+
 export const fetchOffersAction = (): ThunkActionResult =>
   async (dispatch, _getState, api): Promise<void> => {
     const {data} = await api.get<OfferType[]>(APIRoute.Offers);
-    const adaptFromServer = (offer: any) => {
-      const adaptOfferFeatures = (offer: any) => {
-        for (let feature in offer) {
-          if (typeof (offer[feature]) === 'object') {
-            adaptOfferFeatures(offer[feature]);
-          } else {
-            let snakeSymbolIndex = feature.indexOf('_');
-            if (snakeSymbolIndex >= 0) {
-              offer[feature.slice(0, snakeSymbolIndex) + feature.slice(++snakeSymbolIndex)[0].toUpperCase() + feature.slice(++snakeSymbolIndex)] = offer[feature];
-              delete offer[feature];
-            }
-          }
-        }
-      }
-      adaptOfferFeatures(offer);
-      return offer;
-
-    }
     const adaptedOffers = data.map(adaptFromServer);
     console.log(adaptedOffers);
     dispatch(loadOffers(adaptedOffers));
   };
-
-
